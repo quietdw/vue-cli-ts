f3b6dc71c5e0916a8d91dd2e904b8cef13a261c4
quietdw <1044569102@qq.com>
2019-08-10 21:32:17 +0800

    使用ts创建vue class组件，minxins

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld name="xxxxxx"></HelloWorld>
+    {{msg}}
+    {{mixinvalue}}
+    <button @click="xxx">点击</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue, Mixins } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import { MyMixin } from "./mixins/mixin-one";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: { HelloWorld }
 })
-export default class App extends Vue {}
+export default class App extends Mixins(MyMixin) {
+  msg = 123;
+  xxx() {
+    this.msg = 1;
+  }
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+
+  color: red;
 }
 </style>
---
+++ src/mixins/mixin-one.ts
@@ -0,0 +1,8 @@
+
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export class MyMixin extends Vue {
+    mixinvalue = 'mixins'
+}
+
